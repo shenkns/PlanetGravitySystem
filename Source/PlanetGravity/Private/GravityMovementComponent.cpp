6fd55f2aa1e037ab60071f5b5de096bf40303fe2
shenkns <shenkns@gmail.com>
2022-09-14 00:47:55 +0300

    Gravity Movement Component

---
+++ Source/PlanetGravity/Private/GravityMovementComponent.cpp
@@ -0,0 +1,3 @@
+// Copyright shenkns Planet Gravity Developed With Unreal Engine. All Rights Reserved 2022.
+
+#include "GravityMovementComponent.h"
---
+++ Source/PlanetGravity/Public/GravityMovementComponent.h
@@ -0,0 +1,12 @@
+// Copyright shenkns Planet Gravity Developed With Unreal Engine. All Rights Reserved 2022.
+
+#pragma once
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GravityMovementComponent.generated.h"
+
+UCLASS()
+class PLANETGRAVITY_API UGravityMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+};
